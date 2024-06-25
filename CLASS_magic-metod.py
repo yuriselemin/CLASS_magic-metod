14b59791c655f7255649b59b3e4c953f1ec627e8
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-06-25 14:51:35 +0300

    Add files via upload

---
+++ CLASS_magic-metod.py
@@ -0,0 +1,57 @@
+
+
+
+# Конструктор
+__init__(self[, ...])
+
+# Деструктор
+__del__(self)
+
+# Оператор присваивания
+__iadd__(self, other)
+__isub__(self, other)
+__imul__(self, other)
+__itruediv__(self, other)
+__ifloordiv__(self, other)
+__imod__(self, other)
+__ipow__(self, other[, modulo])
+__ilshift__(self, other)
+__irshift__(self, other)
+__iand__(self, other)
+__ior__(self, other)
+__ixor__(self, other)
+
+# Присваивание
+__setattr__(self, name, value)
+
+# Получение атрибута
+__getattribute__(self, name)
+
+# Эквивалентность
+__eq__(self, other)
+__ne__(self, other)
+__lt__(self, other)
+__le__(self, other)
+__gt__(self, other)
+__ge__(self, other)
+
+# Форматирование строки
+__format__(self, format_spec)
+
+# Вывод
+__repr__(self)
+__str__(self)
+
+# Конвертация
+__bytes__(self)
+__hash__(self)
+__bool__(self)
+
+# Наследование
+__new__(cls[, ...])
+__subclasscheck__(subclass)
+__instancecheck__(instance)
+
+
+
+
